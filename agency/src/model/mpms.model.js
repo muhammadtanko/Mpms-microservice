76831ea5af37246686a92f162abe98105c3c325b
muhammadtanko <85347897+muhammadtanko@users.noreply.github.com>
2023-11-09 17:04:02 +0100

    Delete agency directory

--- agency/src/model/mpms.model.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-const agencySchema = new Schema({
-  name: { type: String, index: true, require: true },
-  acronym: { type: String, require: true },
-  logo: { type: String, require: true },
-});
-
-module.exports = model("Agency", agencySchema);
